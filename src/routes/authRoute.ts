79a057ee0275780a96166b8d7c9c49b7e48464c2
zahid-soft <zahidsarang95@gmail.com>
2023-10-20 10:59:50 +0530

    login endpoint

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -7,18 +7,25 @@ import { User } from "../entity/User";
 import { UserService } from "../services/UserService";
 import registerValidators from "../validators/register-validators";
 import { TokenService } from "../services/TokenService";
+import loginValidators from "../validators/login-validators";
+import { CredentialService } from "../services/CredentialService";
 
 const authRouter = express.Router();
 const userRepository = AppDataSource.getRepository(User);
 const userService = new UserService(userRepository);
 const refreshToken = AppDataSource.getRepository(RefreshToken);
 const tokenService = new TokenService(refreshToken);
-const authController = new AuthController(userService, logger, tokenService);
+const credentialsService = new CredentialService();
+const authController = new AuthController(userService, logger, tokenService, credentialsService);
 
 authRouter.post(
     "/register",
     registerValidators,
     (req: Request, res: Response, next: NextFunction) => authController.register(req, res, next),
+);
+
+authRouter.post("/login", loginValidators, (req: Request, res: Response, next: NextFunction) =>
+    authController.login(req, res, next),
 );
 
 export default authRouter;
