58f6303a0426dd2d831899038d8d425d5f3cbb3b
zahid-soft <zahidsarang95@gmail.com>
2023-10-25 20:22:25 +0530

    fixed bug

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -2,7 +2,7 @@ import { NextFunction, Response } from "express";
 import { validationResult } from "express-validator";
 import { Logger } from "winston";
 import { UserService } from "../services/UserService";
-import { RegisterUserRequest } from "../types";
+import { AuthRequest, RegisterUserRequest } from "../types";
 import { TokenService } from "../services/TokenService";
 import { JwtPayload } from "jsonwebtoken";
 import { CredentialService } from "../services/CredentialService";
@@ -144,5 +144,9 @@ export class AuthController {
             return;
         }
     }
-
+    async self(req: AuthRequest, res: Response) {
+        // token req.auth.id
+        const user = await this.userService.findById(Number(req.auth.sub));
+        res.json({ ...user, password: undefined });
+    }
 }
